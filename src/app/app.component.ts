9202aa73de5346d8748d66c70b9cc4d317c27f26
Igor Eskin <eskinhd@gmail.com>
2019-02-24 14:34:29 -0500

    Extracting data and resetting the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,16 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
-  genders = ['male', 'female']
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,7 +44,14 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 
 }
